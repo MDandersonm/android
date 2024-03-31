4e783c9010877527ef9a2d32ac73d79a93c2d10f
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2024-03-31 20:31:04 +0900

    02 textview, 03 imageview

--- app/src/main/java/com/example/knou_android/MainActivity.java
+++ app/src/main/java/com/example/knou_android/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.knou_android;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,8 +16,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.c03_imageview);
+
+        // 상태바 및 탐색 바 색상 변경
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            // 상태바 색상 설정
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.mycolor));
+            // 탐색 바 색상 설정
+            window.setNavigationBarColor(ContextCompat.getColor(this, R.color.mycolor));
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
