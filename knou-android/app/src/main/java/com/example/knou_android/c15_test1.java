10251baaa40bdd7bdfcd1e578cde03aba802351a
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2024-06-15 02:10:07 +0900

    c15 test

---
+++ knou-android/app/src/main/java/com/example/knou_android/c15_test1.java
@@ -0,0 +1,31 @@
+package com.example.knou_android;
+
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class c15_test1 extends AppCompatActivity {
+
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.c15_test1);
+        }
+
+
+
+
+
+}
